280159eb670f80b6ee4ce14e687f65902da47c75
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-08-15 23:01:52 -0300

    ex.a15

---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+
+let mult = +prompt("Digite um número")
+
+for(let i = 1; i <= 10; i++){
+    console.log(mult*i);
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,6 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+for (let i = 0; i < maioresPaises.length; i++) {
+    console.log(`Lista de maiores paise ${i+1} - ${maioresPaises[i]}`);
+    
+}
